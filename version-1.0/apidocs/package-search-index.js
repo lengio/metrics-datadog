d6ab2c59b678f1fc7ed62e5f3d210561be36d9bc
Ricardo Villamil <ricardovj1@gmail.com>
2022-09-22 19:00:38 +0000

    Maven artifacts for 1.0

---
+++ version-1.0/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.coursera.metrics.datadog"},{"l":"org.coursera.metrics.datadog.model"},{"l":"org.coursera.metrics.datadog.transport"},{"l":"org.coursera.metrics.serializer"}];updateSearchResults();
